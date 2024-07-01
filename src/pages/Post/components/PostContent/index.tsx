7583771fd0817146ff1274d94c7a017be0e8d1b5
Victtor777 <victtor.eng.software@gmail.com>
2024-07-01 12:26:47 -0300

    Resolvendo bug para o deploy

--- src/pages/Post/components/PostContent/index.tsx
+++ src/pages/Post/components/PostContent/index.tsx
@@ -1,7 +1,7 @@
 import { PostContentContainer } from "./styles";
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 interface PostContentProps {
   content: string;
@@ -12,24 +12,24 @@ export function PostContent({ content }: PostContentProps) {
     <PostContentContainer>
       <ReactMarkdown
         children={content}
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || "");
-            return !inline && match ? (
-              <SyntaxHighlighter
-                children={String(children).replace(/\n$/, "")}
-                style={dracula as any}
-                language={match[1]}
-                PreTag="div"
-                {...props}
-              />
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            );
-          },
-        }}
+        // components={{
+        //   code({ node, inline, className, children, ...props }) {
+        //     const match = /language-(\w+)/.exec(className || "");
+        //     return !inline && match ? (
+        //       <SyntaxHighlighter
+        //         children={String(children).replace(/\n$/, "")}
+        //         style={dracula as any}
+        //         language={match[1]}
+        //         PreTag="div"
+        //         {...props}
+        //       />
+        //     ) : (
+        //       <code className={className} {...props}>
+        //         {children}
+        //       </code>
+        //     );
+        //   },
+        // }}
       />
     </PostContentContainer>
   );
